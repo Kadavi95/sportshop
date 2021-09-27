6a307d028d229e420c999cc657aa02ac1de4671d
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-09-27 23:26:12 +0200

    upDate

---
+++ sportshop/src/data.js
@@ -0,0 +1,50 @@
+module.exports = function (){
+    return {
+        categories: ["water sports", "football", "chess"],
+        products: [
+            {
+                id: 1, name: "kayak", category: "water sports", 
+                description: "oneman boat", price: 900
+            },
+            {
+                id: 2, name: "life jacket", category: "water sports", 
+                description: "safe and sound", price: 79.95
+            },
+            {
+                id: 3, name: "footer", category: "football", 
+                description: "It has been approved by FIFA", price: 220.50
+            },
+            {
+                id: 4, name: "flags", category: "football", 
+                description: "It makes matches more interesting", price: 450.99
+            },
+            {
+                id: 5, name: "stadion", category: "football", 
+                description: "Is is a small stadion for a litte amount of people", price: 1795000
+            },
+            {
+                id: 6, name: "hat", category: "chess", 
+                description: "It makes you more serious", price: 900
+            },
+            {
+                id: 7, name: "Glossy hair", category: "chess", 
+                description: "Looks", price: 900
+            },
+            {
+                id: 8, name: "chess for cats", category: "chess", 
+                description: "it makes even possible to play with your cat!", price: 900
+            },
+            {
+                id: 9, name: "shining king", category: "chess", 
+                description: "goldened, so amazing", price: 3999
+            },
+        ],
+        orders: []
+
+
+    }
+
+
+
+
+}
